4054c52fb88b4eb025fd120ea8670de66dad718e
Farhah Kamilatun Nuha 36 <farhahkamilatunnuha4803@gmail.com>
2020-02-14 10:41:41 +0700

    first commit

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+<style>
+    footer{
+        font-family: 'Libre Franklin', sans-serif;
+        background-color: grey;
+        color: white;
+        margin-top: 70%;
+        width: 100%;
+        height: 50px;
+    }
+    .container{
+        font-family: 'Libre Franklin', sans-serif;
+        background-color: pink;
+        color: white;
+        width: 100%;
+        height: 50px;
+    }
+    .container1{
+        font-family: 'Libre Franklin', sans-serif;
+        background-color: ;
+        color: white;
+        margin-top: 5px;
+        width: 100%;
+        height: 50px;
+    }
+</style>
+</head>
+<body>
+    <center>
+<footer class="bg-dark text-white">
+    <div class="container">
+      <div class="row">
+        <div class="col text-center" >
+          <p><br>FOOTER</p>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <div>
+<div class="copyright" >
+  <footer class="bg-dark text-white">
+    <div class="container1">
+      <div class="row">
+        <div class="col text-center" >
+          <p ><br>COPYRIGHT</p>
+        </div>
+      </div>
+    </div>
+</div>
+  </footer>
+</div>
+</center>
+</body>
+</html>
---
+++ resources/views/hello.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h2>Hello Matter {{$namaSaya}}</h2><br><br>
+    <form>
+  <div class="form-group row">
+    <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
+    <div class="col-sm-10">
+      <input type="text" readonly class="form-control-plaintext" id="staticEmail" value="email@example.com">
+    </div>
+  </div>
+  <div class="form-group row">
+    <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword">
+    </div>
+  </div>
+</form>
+</body>
+</html>
